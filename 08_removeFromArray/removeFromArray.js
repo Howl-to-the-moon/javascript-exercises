fe184cde03b6f4069659c0a60238b8c947314ad3
Howl-to-the-moon <149625575+Howl-to-the-moon@users.noreply.github.com>
2025-10-27 18:47:17 -0400

    eiefihihihodfsdsfihl

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+
+//the ... makes it so if eed be that argument can be an array
+//of multiple arguements
+const removeFromArray = function(array, ...remove) {
+
+    // the ! flips the .includes on it's head
+
+  return array.filter( item => !remove.includes(item));
+
 };
 
 // Do not edit below this line
