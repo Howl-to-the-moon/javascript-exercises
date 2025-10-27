844f9bfe5f4248cff6e8193a96627ebe80e466d2
Howl-to-the-moon <149625575+Howl-to-the-moon@users.noreply.github.com>
2025-10-27 16:43:51 -0400

    baba

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
+const repeatString = function(word, amount) {
+
+  console.log(word);
+  console.log(amount);
+  let gooby = "";
+
+  if (amount < 0) {
+    return "ERROR";
+  }
+
+    for (let i = 0; i < amount ; i++) {
+       gooby += word;
+    }
+
+    return gooby;
 
 };
+
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
