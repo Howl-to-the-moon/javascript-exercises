95b1ee706fe03d25ce94c52b75103ff85a9a2e8d
Howl-to-the-moon <149625575+Howl-to-the-moon@users.noreply.github.com>
2025-10-28 13:01:40 -0400

    kjkwdkjkhfkskhs

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,31 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+  let santaTracker = false;
+
+  console.log(year);
+
+  if (year % 4 === 0) {
+    
+   santaTracker = true;
+
+  };
+
+  if (year > 99 && year % 100 === 0) {
+
+    if (year % 400 === 0) {
+      return true;
+    }
+
+    santaTracker = false;
+
+  }
+
+
+  return santaTracker;
+
+
+
+  
 
 };
 
