7ff8552f166ff7877f5ead36935e4853dbda5891
Howl-to-the-moon <149625575+Howl-to-the-moon@users.noreply.github.com>
2025-10-28 12:39:37 -0400

    mmmmm

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,47 @@
-const sumAll = function() {
+const sumAll = function(start, end) 
+{
+    let total = 0;
 
-};
+  if  (!Number.isInteger(start) || !Number.isInteger(end) )  {
+  
+  return "ERROR";
+
+ }
+
+ if  (start < 0 || end < 0 )  {
+  
+  return "ERROR";
+
+ }
+
+
+    if (start < end ) 
+      {
+
+        for (let i = start; i <= end ; i ++)
+        {
+          total += i;
+        };
+
+      } else if (end < start) 
+        
+      {
+
+        for (let i = end; i <= start ; i ++)
+          {
+            total += i;
+
+          };
+
+      }
+  
+  return total;
+
+      
+
+}
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
