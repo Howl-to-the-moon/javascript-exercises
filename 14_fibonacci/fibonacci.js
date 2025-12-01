cc377ebcd9df3972bdbc3fc7b7cc1a841d5abe32
Howl-to-the-moon <149625575+Howl-to-the-moon@users.noreply.github.com>
2025-12-01 18:01:50 -0500

    14 done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,40 @@
-const fibonacci = function() {
+const fibonacci = function(fibby) {
+
+
+    //make sure fibby is a numbah
+    fibby = Number(fibby);
+
+    //less than 0 is invalid
+
+    if (fibby < 0){
+        return "OOPS";
+    }
+
+    //idfk why 1 doesn;'t work so uhhhh
+
+    if (fibby == 1) {
+        return 1;
+    }
+
+
+    //honestly more commplicated than it needs to be but it works sooooo
+
+    let former = 0;
+    let latter = 1;
+    let total = 0;
+
+    for (let i = 1; i < fibby; i++) {
+
+     
+
+        total = former + latter;
+        former = latter;
+        latter = total;
+        
+
+    }
+
+    return total;
 
 };
 
