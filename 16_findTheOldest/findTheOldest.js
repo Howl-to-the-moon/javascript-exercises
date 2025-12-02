3f525f2d021b9ab06a23aafd378b08cd1c832f9f
Howl-to-the-moon <149625575+Howl-to-the-moon@users.noreply.github.com>
2025-12-02 13:57:20 -0500

    dinner

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,42 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+
+    //get current date
+     let now = new Date();
+
+     //if no death year, make 'death' year current year
+
+   people.map ( function (item) {
+
+      if (item.yearOfDeath == undefined) {
+        item.yearOfDeath = now.getFullYear();
+        console.log(item.yearOfDeath);
+      }
+
+   });
+
+
+
+   //sorts the people from oldest to youngest
+
+
+    let sortedPeople = people.sort((a, b) => 
+        
+        (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)
+    
+    );
+
+    //returns the first (read: oldest) object in the array
+
+    let lastOneStanding = sortedPeople[0];
+
+    return lastOneStanding;
+
+    
 
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
